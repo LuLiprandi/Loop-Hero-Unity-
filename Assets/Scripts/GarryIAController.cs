13db082b864118050f26da4bf2b4daf52b44b4cd
LuLiprandi <lucie.liprandi@brassart.fr>
2026-02-12 09:34:19 +0100

    IA pt2

--- Assets/Scripts/GarryIAController.cs
+++ Assets/Scripts/GarryIAController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -24,11 +25,49 @@ public class GarryIAControlller : MonoBehaviour
         BehaviourAction();
     }
 
+/// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     private bool TestChangeState()
     {
         switch (state)
         {
+            case StateType.Catch:
+                if(!GetComponent<SightPerception>().isDetected)
+                {
+                    nextState = StateType.Patrole;
+                    return true;
+                }
+                if (Vector3.Distance(target.transform.position, transform.position) > catchDistance)
+                {
+                    nextState = StateType.Follow;
+                    return true;
+                }
+                break;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            case StateType.Patrole:
+                if(GetComponent<SightPerception>().isDetected)
+                {
+                    if (Vector3.Distance(target.transform.position, transform.position) <= catchDistance)
+                    {
+                        nextState = StateType.Catch;
+                        return true;
+
+                    }
+                    nextState = StateType.Follow;
+                    return true;
+                }
+
+                break;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
             case StateType.Follow:
+
+                if(!GetComponent<SightPerception>().isDetected)
+                {
+                    nextState = StateType.Patrole;
+                    return true;
+                }
+
                 if (Vector3.Distance(target.transform.position, transform.position) <= catchDistance);
                 {
                     nextState = StateType.Catch;
@@ -39,6 +78,9 @@ public class GarryIAControlller : MonoBehaviour
         return false;
     }
 
+ ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    
+
     private void ChangeState()
     {
         EndState();
@@ -47,7 +89,18 @@ public class GarryIAControlller : MonoBehaviour
     }
 
     private void StartState()
+
     {
+        switch (state)
+        {
+            case StateType.Patrole:
+                GetComponent<NavMeshAgent>().speed = 2f;
+                break;
+
+            case StateType.Follow:
+                GetComponent<NavMeshAgent>().speed = 4f;
+                break;
+        }
     }
     private void EndState()
     {
@@ -56,7 +109,11 @@ public class GarryIAControlller : MonoBehaviour
             case StateType.Follow:
                 GetComponent<NavMeshAgent>().SetDestination(transform.position);
                 break;
-      
+
+            case StateType.Patrole:
+                GetComponent<NavMeshAgent>().SetDestination(transform.position);
+                break;
+
         }
     }
 
@@ -67,9 +124,11 @@ public class GarryIAControlller : MonoBehaviour
             case StateType.Patrole:
                 PatroleBehaviour();
                 break;
+
             case StateType.Follow:
                 FollowBehaviour();
                 break;
+
             case StateType.Catch:
                 CatchBehaviour();
                 break;
@@ -79,10 +138,12 @@ public class GarryIAControlller : MonoBehaviour
     private void PatroleBehaviour()
     {
         GetComponent<NavMeshAgent>().SetDestination(navepoint.transform.position);
+        GetComponent<Animator>().SetFloat(name: "Speed",GetComponent<NavMeshAgent>().velocity.magnitude);
     }
      private void FollowBehaviour()
     {
         GetComponent<NavMeshAgent>().SetDestination(target.transform.position);
+        GetComponent<Animator>().SetFloat(name: "Speed", GetComponent<NavMeshAgent>().velocity.magnitude);
     }
     private void CatchBehaviour()
     {
@@ -91,4 +152,4 @@ public class GarryIAControlller : MonoBehaviour
     }
 
 
-    }
+}
